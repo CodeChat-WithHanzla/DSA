60bb576a7e24f11c1072a9ebc7b592a8764e1911
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-09-10 10:17:42 +0500

    Sum of even number from 1 - N And fix a bug and we don`t need & becoz we are not modifying !

--- Conditional.cpp
+++ Conditional.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 using namespace std;
-void characterSpecifier(char &ch);
+void characterSpecifier(char ch);
+void sumOfEven(int n);
+
 int main()
 {
-    char ch;
-    cout << "Enter the Character : ";
-    cin >> ch;
-    characterSpecifier(ch);
+
+    int n = 10;
+    sumOfEven(n);
 }
 
-
-
-void characterSpecifier(char &ch)
+void characterSpecifier(char ch)
 {
     if (ch >= 'a' && ch <= 'z')
         cout << "Lower Case" << endl;
     else if (ch >= 'A' && ch <= 'Z')
         cout << "Upper Case" << endl;
-    else if (ch >= '1' && ch <= '9')
+    else if (ch >= '0' && ch <= '9')
         cout << "Numeric Character" << endl;
     else
         cout << "Unknown to me ! " << endl;
+}
+void sumOfEven(int n)
+{
+    int i = 1, sum = 0;
+    while (i <= n)
+    {
+        if (i % 2 == 0)
+            sum += i;
+        i++;
+    }
+    cout << "Sum of even numbers from 1" << " to " << n << " is : " << sum << endl;
 }
