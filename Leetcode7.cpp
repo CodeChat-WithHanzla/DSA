6b18d904059179ad6d57ffe22e15dfc70168db93
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-09-13 11:20:51 +0500

    Update the leetcoode problem 7

--- Leetcode7.cpp
+++ Leetcode7.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include <cmath>
+#include <climits>
 class Solution
 {
 public:
     int reverse(int x)
     {
         int reverseInt = 0;
+
         while (x != 0)
         {
+            if (reverseInt > INT_MAX / 10 || reverseInt < INT_MIN / 10)
+                return 0;
             int digit = x % 10;
-            reverseInt = reverseInt*10 + digit;
-            x/=10;
+            reverseInt = reverseInt * 10 + digit;
+            x /= 10;
         }
         return reverseInt;
     }
