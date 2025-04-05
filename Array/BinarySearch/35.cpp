3116059e06296e531e838ed9079711ef28014d27
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2025-04-05 22:21:19 +0500

    find and search using binary search algo

---
+++ Array/BinarySearch/35.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        sort(nums.begin(), nums.end());
+        int start = 0, end = nums.size() - 1;
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (target > nums[mid])
+            {
+                start = mid + 1;
+            }
+            else
+                end = mid - 1;
+        }
+        return start;
+    }
+};
