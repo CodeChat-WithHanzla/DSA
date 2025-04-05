36db64cef763067ec4c8a331b266f89fc5894c55
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2025-04-05 21:03:31 +0500

    solving 2 sum using unordered map

--- Array/Leetcode_1.cpp
+++ Array/Leetcode_1.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 class Solution
 {
@@ -7,6 +6,7 @@ public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
         vector<int> ans;
+        /*
         for (int i = 0; i < nums.size(); i++)
         {
             for (int j = i + 1; j < nums.size(); j++)
@@ -18,7 +18,17 @@ public:
                     return ans;
                 }
             }
+        }*/
+        unordered_map<int, int> seen;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int compliment = target - nums[i];
+            if (seen.find(compliment) != seen.end())
+            {
+                return {seen[compliment], i};
+            }
+            seen[nums[i]] = i;
         }
-        return ans;
+        return {};
     }
 };
